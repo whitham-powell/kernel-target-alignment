7fed88483bc268f10ca3ce95285001dc1a781473
Elijah Whitham-Powell <20101453+whitham-powell@users.noreply.github.com>
2025-06-08 12:47:40 -0700

    intial pre-commit added and pyproject config

--- src/kta.py
+++ src/kta.py
@@ -1,14 +1,12 @@
 # src/kta.py
 
 import numpy as np
-
 from numpy.typing import NDArray
 
 
 def kta(K: NDArray, y: NDArray) -> float:
     y = y.ravel()
-    Y = np.outer(y,y)
-    numerator =  np.sum(K * Y)
+    Y = np.outer(y, y)
+    numerator = np.sum(K * Y)
     denominator = np.linalg.norm(K) * np.linalg.norm(Y)
     return float(numerator / denominator)
-
